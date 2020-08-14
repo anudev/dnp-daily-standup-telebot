1dfa3bcbc227393e3345f96317624eb9af220378
adunaev <anton.dunaev@leroymerlin.ru>
2020-08-15 02:06:02 +0300

    remove unused config field

--- config.js
+++ config.js
@@ -1,4 +1,3 @@
 module.exports = {
-    botToken: '1116468884:AAF5wxUm75UzaG9uVmhipzwCeD_8arUQfFI',
-    adminChatId: 12345678,       //your telegram chat id here, allows usage of /broadcast command
+    botToken: '1116468884:AAF5wxUm75UzaG9uVmhipzwCeD_8arUQfFI'
 }
