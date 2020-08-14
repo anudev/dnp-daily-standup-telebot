90ccdf264a79c05a8737a1fca648bab45f018fd5
adunaev <anton.dunaev@leroymerlin.ru>
2020-08-14 03:28:08 +0300

    first functional variant

--- bot.js
+++ bot.js
@@ -1,9 +1,4 @@
 const Telegraf = require('telegraf');
-const {
-    Extra,
-    Markup
-} = require('telegraf');
-
 const config = require('./config');
 const dataService = require('./dataService');
 
@@ -11,35 +6,11 @@ const bot = new Telegraf(config.botToken);
 
 const helpMsg = `Command reference:
 /start - Start bot (mandatory in groups)
-/inc - Increment default counter
-/inc1 - Increment counter 1
-/incx - Increment counter x (replace x with any number)
-/dec - Decrement counter
-/decx - Decrement counter x
-/reset - Reset counter back to 0
-/resetx - Reset counter x back to 0
-/set y - Set counter to y [/set y]
-/setx y - Set counter x to y [/setx y]
-/get - Show current counter
-/getx - Show value of counter x
-/getall - Show all counters
-/stop - Attemt to stop bot
+/startStandUp - Print random list of users
 /about - Show information about the bot
-/help - Show this help page
+/help - Show this help page`;
 
-Tip: You can also use e.g. '/inc2 5' to increase counter two by five counts.`;
-
-const inputErrMsg = `💥 BOOM... 🔩☠🔧🔨⚡️
-Hm, that wasn't supposed to happen. You didn't input invalid characters, did you?
-The usage for this command is \"/set x\", where x is a number.
-At the moment, I can only count integers, if you want to add your own number system, please feel free to do so. Just click here: /about `;
-
-const incNMsg = `To use multiple counters, simply put the number of the counter you want to increase directly after the command like so:
-/inc1 <- this will increment counter 1
-/inc  <- this will increment the default counter (0)
-This does also work with other commands like /dec1 /reset1 /set1 /get1`;
-
-const aboutMsg = "This bot was created by @LeoDJ\nSource code and contact information can be found at https://github.com/LeoDJ/telegram-counter-bot";
+const aboutMsg = "This bot was created by @AnuDev\nSource code and contact information can be found at https://github.com/anudev/dnp-daily-standup-telebot";
 
 function getRegExp(command) {
     return new RegExp("/" + command + "[0-9]*\\b");
@@ -50,8 +21,6 @@ bot.telegram.getMe().then((botInfo) => {
     bot.options.username = botInfo.username;
     console.log("Initialized", botInfo.username);
 });
-
-dataService.loadUsers();
 
 function userString(ctx) {
     return JSON.stringify(ctx.from.id == ctx.chat.id ? ctx.from : {
@@ -71,27 +40,27 @@ function logOutMsg(ctx, text) {
     }, text);
 }
 
-bot.command('broadcast', ctx => {
-    if (ctx.from.id == config.adminChatId) {
-        var words = ctx.message.text.split(' ');
-        words.shift(); //remove first word (which ist "/broadcast")
-        if (words.length == 0) //don't send empty message
-            return;
-        var broadcastMessage = words.join(' ');
-        var userList = dataService.getUserList();
-        console.log("Sending broadcast message to", userList.length, "users:  ", broadcastMessage);
-        userList.forEach(userId => {
-            console.log(">", {id: userId}, broadcastMessage);
-            ctx.telegram.sendMessage(userId, broadcastMessage);
-        });
+const users = new Set(['@aaaa', '@bbbb']);
+
+bot.hears(message => !/\/(start|startStandUp|help|about)/.test(message),
+    ctx => users.add(`@${ctx.message.from.username}`));
+
+bot.command('startStandUp', ctx => {
+    logMsg(ctx);
+    logOutMsg(ctx, aboutMsg);
+    const userForSort = [];
+    for (let user of users.keys()) {
+        userForSort.push({sortKey: Math.floor(Math.random() * 10000), user: user})
     }
+    userForSort.sort((a, b) => a.sortKey - b.sortKey);
+    ctx.reply(userForSort.map((val, index, array) => `${index} ${val.user}`).join('\n'));
 });
 
-bot.command('start', ctx => {
+bot.start(ctx => {
     logMsg(ctx);
     dataService.registerUser(ctx);
     dataService.setCounter(ctx.chat.id, '0', 0);
-    var m = "Hello, I'm your personal counter bot, simply use the commands to control the counter";
+    var m = "Hello, I'm daily standUp support bot.";
     ctx.reply(m);
     logOutMsg(ctx, m);
     setTimeout(() => {
@@ -100,20 +69,7 @@ bot.command('start', ctx => {
     }, 50); //workaround to send this message definitely as second message
 });
 
-bot.command('stop', ctx => {
-    logMsg(ctx);
-    var m = "I'm sorry, Dave, I'm afraid I can't do that.";
-    logOutMsg(ctx, m);
-    ctx.reply(m);
-});
-
-bot.command(['incx', 'decx', 'getx', 'setx', 'resetx'], ctx => {
-    logMsg(ctx);
-    logOutMsg(ctx, incNMsg);
-    ctx.reply(incNMsg);
-});
-
-bot.command('help', ctx => {
+bot.help(ctx => {
     logMsg(ctx);
     logOutMsg(ctx, helpMsg);
     ctx.reply(helpMsg);
@@ -125,112 +81,7 @@ bot.command('about', ctx => {
     ctx.reply(aboutMsg);
 });
 
-bot.command('getall', ctx => {
-    logMsg(ctx);
-    counters = dataService.getAllCounters(ctx.chat.id);
-    msg = "";
-    Object.keys(counters).forEach(counterId => {
-        msg += '[' + counterId + '] ' + counters[counterId].value + "\n";
-    });
-    logOutMsg(ctx, msg);
-    ctx.reply(msg);
-});
-
-bot.hears(getRegExp('inc'), ctx => {
-    logMsg(ctx);
-    currentCommand = 'inc';
-    var m = ctx.message.text.match(getRegExp(currentCommand))[0]; //filter command
-    var counterId = m.substring(m.indexOf(currentCommand) + currentCommand.length) || 0; //get id of command, return 0 if not found
-
-    var delta = 1;
-    params = ctx.message.text.split(" ");
-    if (params.length == 2 && !isNaN(params[1])) {
-        delta = Math.floor(params[1]);
-    }
-
-    var val = +dataService.getCounter(ctx.chat.id, counterId);
-    val += delta;
-    dataService.setCounter(ctx.chat.id, counterId, val);
-
-    var printCounterId = counterId ? "[" + counterId + "] " : "";
-    val = printCounterId + val;
-    logOutMsg(ctx, val);
-    ctx.reply(val);
-});
-
-bot.hears(getRegExp('dec'), ctx => {
-    logMsg(ctx);
-    currentCommand = 'dec';
-    var m = ctx.message.text.match(getRegExp(currentCommand))[0]; //filter command
-    var counterId = m.substring(m.indexOf(currentCommand) + currentCommand.length) || 0; //get id of command, return 0 if not found
-
-    var delta = 1;
-    params = ctx.message.text.split(" ");
-    if (params.length == 2 && !isNaN(params[1])) {
-        delta = Math.floor(params[1]);
-    }
-
-    var val = +dataService.getCounter(ctx.chat.id, counterId);
-    val -= delta;
-    dataService.setCounter(ctx.chat.id, counterId, val);
-
-    var printCounterId = counterId ? "[" + counterId + "] " : "";
-    val = printCounterId + val;
-    logOutMsg(ctx, val);
-    ctx.reply(val);
-});
-
-bot.hears(getRegExp('reset'), ctx => {
-    logMsg(ctx);
-    currentCommand = 'reset';
-    var m = ctx.message.text.match(getRegExp(currentCommand))[0]; //filter command
-    var counterId = m.substring(m.indexOf(currentCommand) + currentCommand.length) || 0; //get id of command, return 0 if not found
-
-    var val = 0;
-    dataService.setCounter(ctx.chat.id, counterId, val);
-
-    var printCounterId = counterId ? "[" + counterId + "] " : "";
-    val = printCounterId + val;
-    logOutMsg(ctx, val);
-    ctx.reply(val);
-});
-
-bot.hears(getRegExp('get'), ctx => {
-    logMsg(ctx);
-    currentCommand = 'get';
-    var m = ctx.message.text.match(getRegExp(currentCommand))[0]; //filter command
-    var counterId = m.substring(m.indexOf(currentCommand) + currentCommand.length) || 0; //get id of command, return 0 if not found
-
-    var val = +dataService.getCounter(ctx.chat.id, counterId);
-
-    var printCounterId = counterId ? "[" + counterId + "] " : "";
-    val = printCounterId + val;
-    logOutMsg(ctx, val);
-    ctx.reply(val);
-});
-
-bot.hears(getRegExp('set'), ctx => {
-    logMsg(ctx);
-    currentCommand = 'set';
-    var m = ctx.message.text.match(getRegExp(currentCommand))[0]; //filter command
-    var counterId = m.substring(m.indexOf(currentCommand) + currentCommand.length) || 0; //get id of command, return 0 if not found
-
-    params = ctx.message.text.split(" ");
-    if (params.length == 2 && !isNaN(params[1])) {
-        var val = Math.floor(params[1]);
-        dataService.setCounter(ctx.chat.id, counterId, val);
-        var printCounterId = counterId ? "[" + counterId + "] " : "";
-        val = printCounterId + val;
-    } else {
-        val = inputErrMsg;
-    }
-
-    logOutMsg(ctx, val);
-    ctx.reply(val);
-});
-
 
 bot.startPolling();
-
 
 module.exports = {}
--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 module.exports = {
-    botToken: 'YOURTOKENGOESHERE',
+    botToken: '1116468884:AAF5wxUm75UzaG9uVmhipzwCeD_8arUQfFI',
     adminChatId: 12345678,       //your telegram chat id here, allows usage of /broadcast command
 }
