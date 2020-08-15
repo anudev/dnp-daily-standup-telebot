60b88d0364c794db48e5c6bc533c63049d89d8f4
adunaev <anton.dunaev@leroymerlin.ru>
2020-08-16 01:17:57 +0300

    add dummy http server on port from env

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
+var http = require('http');
+
+http.createServer(function (req, res) {
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end("Hello :)");
+}).listen(process.env.PORT || 3000);
+
 const bot = require('./bot');
